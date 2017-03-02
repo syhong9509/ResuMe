b9f80062ac21ba006eeecd8f28f3a6c07c929546
NickNIU <yniu24@uwo.ca>
2017-03-02 15:58:02 -0500

    facespace version useraccount

--- grails-app/controllers/resume/UserAccountController.groovy
+++ grails-app/controllers/resume/UserAccountController.groovy
@@ -1,107 +1,40 @@
 package resume
 
+import grails.rest.RestfulController
+
 import static org.springframework.http.HttpStatus.*
-import grails.transaction.Transactional
 
-@Transactional(readOnly = true)
-class UserAccountController {
 
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
-    def index(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        respond UserAccount.list(params), model:[userAccountCount: UserAccount.count()]
+class UserAccountController extends RestfulController{
+
+    static allowedMethods = [createAccount: 'POST']
+    static responseFormats = ['json', 'xml']
+
+    UserAccountController(){
+        super(UserAccount)
     }
 
-    def show(UserAccount userAccount) {
-        respond userAccount
+    def index() {
+        // some default behaviour could be here
     }
 
-    def create() {
-        respond new UserAccount(params)
-    }
-
-    @Transactional
-    def save(UserAccount userAccount) {
-        if (userAccount == null) {
-            transactionStatus.setRollbackOnly()
-            notFound()
-            return
+    // Our first POST-enabled method
+    // We just want to get the status text and save it to the profile.
+    def create(){
+        def uname = params.userName
+        def upassword = params.password
+        def account = UserAccount.find{userName == uname}
+        if(account == null){
+            account = new UserAccount(userName: uname, password: upassword)
+            new Profile(ownerAccount: account).save()
+            System.out.print('created')
+            response.status = 200
         }
-
-        if (userAccount.hasErrors()) {
-            transactionStatus.setRollbackOnly()
-            respond userAccount.errors, view:'create'
-            return
-        }
-
-        userAccount.save flush:true
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.created.message', args: [message(code: 'userAccount.label', default: 'UserAccount'), userAccount.id])
-                redirect userAccount
-            }
-            '*' { respond userAccount, [status: CREATED] }
-        }
-    }
-
-    def edit(UserAccount userAccount) {
-        respond userAccount
-    }
-
-    @Transactional
-    def update(UserAccount userAccount) {
-        if (userAccount == null) {
-            transactionStatus.setRollbackOnly()
-            notFound()
-            return
-        }
-
-        if (userAccount.hasErrors()) {
-            transactionStatus.setRollbackOnly()
-            respond userAccount.errors, view:'edit'
-            return
-        }
-
-        userAccount.save flush:true
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'userAccount.label', default: 'UserAccount'), userAccount.id])
-                redirect userAccount
-            }
-            '*'{ respond userAccount, [status: OK] }
-        }
-    }
-
-    @Transactional
-    def delete(UserAccount userAccount) {
-
-        if (userAccount == null) {
-            transactionStatus.setRollbackOnly()
-            notFound()
-            return
-        }
-
-        userAccount.delete flush:true
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: 'userAccount.label', default: 'UserAccount'), userAccount.id])
-                redirect action:"index", method:"GET"
-            }
-            '*'{ render status: NO_CONTENT }
-        }
-    }
-
-    protected void notFound() {
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'userAccount.label', default: 'UserAccount'), params.id])
-                redirect action: "index", method: "GET"
-            }
-            '*'{ render status: NOT_FOUND }
+        else{
+            // Find a better response code...
+            System.out.print('already there')
+            response.status = 404
         }
     }
 }
